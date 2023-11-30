f1b14746ec6a821aaf01d38b4dd4611968d5776a
VincentBerdillon <vincent.berdillon@gmail.com>
2023-11-30 14:50:15 +0100

    config/router,store,reducers,hooks,component App&Home

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './components/App/App';
+import Home from './components/Home/Home';
+
+// eslint-disable-next-line import/prefer-default-export
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
