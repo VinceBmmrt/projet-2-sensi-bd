14e46b419fbaf9514ce248f9585f5ece38f084a6
VincentBerdillon <vincent.berdillon@gmail.com>
2023-12-18 13:55:30 +0100

    Add comments to html, main, and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// ReactDom permet d'injecter notre application dans le DOM
 import ReactDOM from 'react-dom/client';
 
 import './styles/index.scss';
@@ -7,10 +8,16 @@ import { RouterProvider } from 'react-router-dom';
 import store from './store';
 import { router } from './routes';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+// Création d'un élément root pour mon application
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// On injecte notre application dans le DOM
+// On utilise un RouterProvider pour aiguiller notre application en fonction de l'url, chaque url correspond à un composant définis dans le `router`
+root.render(
+  <Provider store={store}>
+    {/* on englobe l'application du provider de react-redux afin de fournir les données du store */}
+    <RouterProvider router={router} />
+  </Provider>
 );
